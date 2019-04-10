43cef81341637b3c89bf01ecb5c5f1a7bbe40c45
mcamberlin <merlin.camberlin@student.uclouvain.be>
2019-04-10 12:18:20 +0200

    Premier jet de la lecture de fichier

--- cracker.c
+++ cracker.c
@@ -1,7 +1,54 @@
+/*
+	Fichier cracker.c réprésente le code dans son intégralité
+	
+	Auteurs: 
+		- CAMBERLIN Merlin
+		- PISVIN Arthur
+	Version: 10-09-19
+
+	Commandes à indiquer dans le compilateur:
+		- cd "~/Documents/LSINF1252-PasswordCracker-Gr118-2019"
+		- gcc -o cracker cracker.c
+		- ./cracker
+
+	Commandes git:
+		- git status
+		- git add
+		- git commit -m "blablabla" monfichier
+		- git push
+	Remarques:
+		- Ne pas oublier de faire les docstring à chaque fois
+*/
+
+
+
 #include <stdio.h>  
 #include <stdlib.h>
 
-int main() {
+/** La fonction main est la fonction principale de notre programme:
+	@pre - argc = nombre d'arguments passés lors de l'appel de l'exécutable
+		- argv = tableau de pointeurs reprenant les arguments donnés par l'exécutable
+	@post - exécute les différentes instructions du code
+	
+*/
+int main(int argc, char *argv[]) {
+
+// 1ere étape : lecture des fichiers binaires
+
+	
+	// Initialisation un thread par type de fichier
+	// Un pour ceux provenant d'internet
+	// Un pour ceux provenant du disque dur
+	// ...
+
+	
+
+	
+	
+	
+	
+	
+
 
         printf ("Hello, World!\n"); 
         return EXIT_SUCCESS; 
