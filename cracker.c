44a52e90c6308e803624294647649b39b8b458aa
mcamberlin <merlin.camberlin@student.uclouvain.be>
2019-04-10 15:58:05 +0200

    Lecture des commandes

--- cracker.c
+++ cracker.c
@@ -7,7 +7,7 @@
 	Version: 10-09-19
 
 	Commandes à indiquer dans le compilateur:
-		- cd "~/Documents/LSINF1252-PasswordCracker-Gr118-2019"
+		- cd ~/Documents/LSINF1252-PasswordCracker-Gr118-2019
 		- gcc -o cracker cracker.c
 		- ./cracker arg1 arg2 arg3
 
@@ -15,6 +15,7 @@
 		- git status
 		- git add monfichier
 		- git commit -m "blablabla" monfichier
+		- git commit -i -m "blablabla" monfichier // en cas de fusion de conflits
 		- git push
 	Remarques:
 		- Ne pas oublier de faire les docstring à chaque fois
@@ -27,13 +28,6 @@
 #include <string.h> // include pour utiliser la fonction strstr() semblalbe à contains()
 
 
-
-//Arguments des fonctions
-#define arg_t = "-t';
-#define arg_c = "-c";
-#define arg_o = "-o";
-
-
 /** La fonction main est la fonction principale de notre programme:
 	@pre - argc = nombre d'arguments passés lors de l'appel de l'exécutable
 		- argv = tableau de pointeurs reprenant les arguments donnés par l'exécutable
@@ -44,38 +38,44 @@ int main(int argc, char *argv[]) {
 
 // tête de la fonction : ./cracker [-t NTHREADS] [-c] [-o FICHIEROUT] FICHIER1 [FICHIER2 ... FICHIERN]
 
-	/* 1ere étape : lecture des fichiers binaires
-	Initialisation un thread par type de fichier
-	Un pour ceux provenant d'internet
-	Un pour ceux provenant du disque dur
-	...
+	/* 1e étape : lecture des fichiers binaires
+			Initialisation un thread par type de fichier
+			Un pour ceux provenant d'internet
+			Un pour ceux provenant du disque dur
+			...
 	*/
 
 
-	// 1.1 lecture des arguments de la commande de l'exécutable:
-	if(argc ==1)
-	{
-		printf("Aucun argument de la fonction n'a été donné \n");
-	}
-	else
-	{
-		printf("argc: %d \n",argc);
-		printf("Le nom de l'exécutable est : %s \n",argv[0]);
-		for (int i=1; i < argc; i++) 
-		{
-			printf("Argument %d : %s \n", i+1, argv[i]);
-		}	
-	}
+	// 1.1 lecture des arguments de la commande de l'exécutable
 	
-	int nbrThreadCalcul = 1; // par défaut, il vaut 1
-	int occurenceVoyelles = 1; // par défaut, le critère de sélection est sur le nombre d'occurences des voyelles
-	char[] nomFichierSortie;
+	//Arguments des fonctions
+	char arg_t[] = "-t";
+	char arg_c[] = "-c";
+	char arg_o[] = "-o";
 
-	if( strstr(&argv,arg_t) == NULL) //char *strstr(const char *haystack, const char *needle)
+	//Valeurs par défaut
+	int nbreThreadCalcul = 1;
+
+	printf("%d-1 arguments spécifié(s) \n",argc);
+
+	for (int i=1; i < argc; i++) 
 	{
+		if( strstr(argv[i],arg_t) != NULL)
+		{// cas où l'exécutable spécifie l'argument -t
+			printf("On a spécifié -t \n");
+			nbreThreadCalcul = (int) *argv[2];
+			printf("Le nombre de thread de calcul est : %d \n",nbreThreadCalcul);
+		}   
 		
-	}
-	 
+	}	
+ 
+
+	/*fichier = fopen (argv[1], "r");  On ouvre le fichier dont le chemin est accessible via argv[1] 
+	*/
+
+
+
+
 
 	/* 2e étape : création du tableau contenant les hash (taille : il avait dit +/- 2 fois le nombre de
 	threads de calcul) DO NOT FORGET -> 2 sémaphores et 1 mutex
@@ -98,6 +98,6 @@ int main(int argc, char *argv[]) {
 	FICHIEROUT la liste chainée qu'il reste
 	*/
 
-        printf ("Hello, World!\n"); 
+        printf ("\n \n \nFin du programme...\n"); 
         return EXIT_SUCCESS; 
 }
